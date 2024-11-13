f1b5f9fcbee04c1f97942b27cba97a7c059dc182
yongchane <202021113@sangmyung.kr>
2024-11-13 15:29:01 +0900

    파일 이름 변경

---
+++ Nutritional_project/src/page/main/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import CheckBox from "./components/CheckBox";
+import ShowNew from "./components/ShowNews";
+
+const index = () => {
+  return (
+    <div className="health-container">
+      <div className="health-header-container">
+        <div className="health-header-title">Biomatch</div>
+        <div className="health-header-condition-container">
+          <div className="health-header-condition">
+            어제에 비해 <br />
+            컨디션이 좋아요!
+          </div>
+        </div>
+      </div>
+      <CheckBox
+        title={"나의 건강 상태 측정하기"}
+        content={"BMI를 측정하여 나의 건강상태를 확인해 보세요"}
+      />
+      <CheckBox
+        title={"영양제 추천 "}
+        content={"AI 채팅을 통해서 영양제를 추천 받아 보아요"}
+      />
+      <div className="health-news-container-title">건강 매거진</div>
+      <ShowNew />
+    </div>
+  );
+};
+
+export default index;
